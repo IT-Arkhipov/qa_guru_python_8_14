d35675f753876731c73744bca66cdc32d48a0e32
Andrei Arkhipov <135824709+IT-Arkhipov@users.noreply.github.com>
2023-12-31 19:29:53 +0300

    Update attach.py

--- utils/attach.py
+++ utils/attach.py
@@ -18,7 +18,7 @@ def add_html(browser):
 
 
 def add_video(browser):
-    video_url = "http://172.17.0.1:4444/video/" + browser.driver.session_id + ".mp4"
+    video_url = "http://127.0.0.1/video/" + browser.driver.session_id + ".mp4"
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
            + "' type='video/mp4'></video></body></html>"
